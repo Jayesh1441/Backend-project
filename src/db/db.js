a7a06bbe9997851d3aa066060d40aedefeb23552
jayesh1441 <jayeshghongade007@gmail.com>
2024-04-05 20:15:52 +0530

    connected to DB

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,9 @@
-import { Mongoose } from "mongoose";
-import { DB_NAME } from "../constants";
+import { mongoose } from "mongoose";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await Mongoose.console(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
         console.log(`MongoDB connected !! HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.error("Error connecting to the database: ", error);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import Mongoose from "mongoose";
-import DB_NAME from "./constants";
-import connectDB from "./db/db";
+// require('dotenv').config({ path: "./env" });
+import dotenv from "dotenv"
+
+import connectDB from "./db/db.js";
+
+dotenv.config({ path: "./.env" })
 
 connectDB();
 
